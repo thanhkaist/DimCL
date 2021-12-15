3be6c2d8862840dbbd34910d76356a0762f85f41
trung <trungpx@kaist.ac.kr>
2021-12-15 21:35:02 +0900

    update

--- solo/losses/oursloss.py
+++ solo/losses/oursloss.py
@@ -20,10 +20,72 @@
 import torch
 import torch.nn.functional as F
 from solo.utils.misc import gather, get_rank
+from typing import Optional
 import ipdb
 
-### Our Loss defined here
-def ours_loss_func(z1: torch.Tensor, z2: torch.Tensor, indexes: torch.Tensor, tau_decor: float = 0.1) -> torch.Tensor:
+### our loss function for single gpu
+def ours_loss_func( z1: torch.Tensor, z2: torch.Tensor, indexes: torch.Tensor, tau_decor: float = 0.1, extra_pos_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+    
+
+    """Computes SimCLR's loss given batch of projected features z1 from view 1 and
+    projected features z2 from view 2.
+
+    Args:
+        z1 (torch.Tensor): NxD Tensor containing projected features from view 1.
+        z2 (torch.Tensor): NxD Tensor containing projected features from view 2.
+        temperature (float): temperature factor for the loss. Defaults to 0.1.
+        extra_pos_mask (Optional[torch.Tensor]): boolean mask containing extra positives other
+            than normal across-view positives. Defaults to None.
+
+    Returns:
+        torch.Tensor: SimCLR loss.
+    """
+
+    device = z1.device
+
+    # z1 = F.normalize(z1, dim=-1)
+    # z2 = F.normalize(z2, dim=-1)
+
+    N, D = z1.size()
+
+    # to match the original code
+    bn = torch.nn.BatchNorm1d(D, affine=False).to(z1.device)
+    z1 = bn(z1).T
+    z2 = bn(z2).T
+
+
+    b = z1.size(0)
+    z = torch.cat((z1, z2), dim=0)
+
+    z = F.normalize(z, dim=-1)
+
+    logits = torch.einsum("if, jf -> ij", z, z) / tau_decor
+    logits_max, _ = torch.max(logits, dim=1, keepdim=True)
+    logits = logits - logits_max.detach()
+
+    # positive mask are matches i, j (i from aug1, j from aug2), where i == j and matches j, i
+    pos_mask = torch.zeros((2 * b, 2 * b), dtype=torch.bool, device=device)
+    pos_mask[:, b:].fill_diagonal_(True)
+    pos_mask[b:, :].fill_diagonal_(True)
+
+    # if we have extra "positives"
+    if extra_pos_mask is not None:
+        pos_mask = torch.bitwise_or(pos_mask, extra_pos_mask)
+
+    # all matches excluding the main diagonal
+    logit_mask = torch.ones_like(pos_mask, device=device).fill_diagonal_(0)
+
+    exp_logits = torch.exp(logits) * logit_mask
+    log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+    # compute mean of log-likelihood over positives
+    mean_log_prob_pos = (pos_mask * log_prob).sum(1) / pos_mask.sum(1)
+    # loss
+    loss = -mean_log_prob_pos.mean()
+    return loss
+
+### our loss function for multiple gpu
+def ours_loss_func_multigpu(z1: torch.Tensor, z2: torch.Tensor, indexes: torch.Tensor, tau_decor: float = 0.1) -> torch.Tensor:
     """Computes Ours's loss given batch of projected features z
     from different views, a positive boolean mask of all positives and
     a negative boolean mask of all negatives.
