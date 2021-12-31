7e81f6c9d2eb46423043573c6e9232f853882ecf
trungpx <trungpx@kaist.ac.kr>
2021-12-31 16:54:20 +0900

    update imagenet ours bash files

--- bash_files/pretrain/imagenet/byol.sh
+++ bash_files/pretrain/imagenet/byol.sh
@@ -15,8 +15,8 @@ python3 ../../../main_pretrain.py \
     --eta_lars 0.001 \
     --exclude_bias_n_norm \
     --scheduler warmup_cosine \
-    --lr 0.45 \
-    --classifier_lr 0.2 \
+    --lr 4.0 \
+    --classifier_lr 1.0 \
     --accumulate_grad_batches 16 \
     --weight_decay 1e-6 \
     --batch_size 64 \
@@ -28,7 +28,7 @@ python3 ../../../main_pretrain.py \
     --gaussian_prob 1.0 0.1 \
     --solarization_prob 0.0 0.2 \
     --num_crops_per_aug 1 1 \
-    --name byol_res50_8gpu_bsz64 \
+    --name byol_res50_ours_8gpu_bsz64 \
     --project Imagenet1K-200ep \
     --entity kaistaim \
     --wandb \
@@ -41,7 +41,55 @@ python3 ../../../main_pretrain.py \
     --final_tau_momentum 1.0 \
     --lam 0.1 \
     --tau_decor 0.1 \
-    --our_loss False \
+    --our_loss True \
     --knn_eval \
     --keep_previous_checkpoints \
     --checkpoint_frequency 5 \
+
+# python3 ../../../main_pretrain.py \
+#     --dataset imagenet \
+#     --backbone resnet50 \
+#     --data_dir ~/workspace/datasets/ \
+#     --train_dir imagenet/train \
+#     --val_dir imagenet/val \
+#     --max_epochs 200 \
+#     --gpus 0,1,2,3,4,5,6,7 \
+#     --accelerator gpu \
+#     --strategy ddp \
+#     --sync_batchnorm \
+#     --precision 16 \
+#     --optimizer sgd \
+#     --lars \
+#     --eta_lars 0.001 \
+#     --exclude_bias_n_norm \
+#     --scheduler warmup_cosine \
+#     --lr 0.45 \
+#     --classifier_lr 0.2 \
+#     --accumulate_grad_batches 16 \
+#     --weight_decay 1e-6 \
+#     --batch_size 64 \
+#     --num_workers 4 \
+#     --brightness 0.4 \
+#     --contrast 0.4 \
+#     --saturation 0.2 \
+#     --hue 0.1 \
+#     --gaussian_prob 1.0 0.1 \
+#     --solarization_prob 0.0 0.2 \
+#     --num_crops_per_aug 1 1 \
+#     --name byol_res50_8gpu_bsz64 \
+#     --project Imagenet1K-200ep \
+#     --entity kaistaim \
+#     --wandb \
+#     --save_checkpoint \
+#     --method byol \
+#     --proj_output_dim 256 \
+#     --proj_hidden_dim 4096 \
+#     --pred_hidden_dim 4096 \
+#     --base_tau_momentum 0.99 \
+#     --final_tau_momentum 1.0 \
+#     --lam 0.1 \
+#     --tau_decor 0.1 \
+#     --our_loss False \
+#     --knn_eval \
+#     --keep_previous_checkpoints \
+#     --checkpoint_frequency 5 \
